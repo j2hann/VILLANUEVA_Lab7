67dd877dc549b6e1d6759b2af6c818b068e6d3ad
Johann Villanueva <villanueva.johann06@gmail.com>
2024-10-16 15:39:15 +0800

    SUPER DUPER UPDATED NA TALAGA

--- VILLANUEVA_Lab7.py
+++ VILLANUEVA_Lab7.py
@@ -23,34 +23,24 @@ else:
             print()
             print("Hello, " + name + "!")
             print("Section: " + section)
-
+            print("Congratulations. Your final grade is " + str(averageGrade))
             if 99 <= averageGrade <= 100:
                 print("Excellent! Your grade point is 1.00")
-                print("Your final grade is " + str(averageGrade))
             elif 96 <= averageGrade <= 98:
                 print("Outstanding! Your grade point is 1.25")
-                print("Your final grade is " + str(averageGrade))
             elif 93 <= averageGrade <= 95:
                 print("Superior! Your grade point is 1.50")
-                print("Your final grade is " + str(averageGrade))
             elif 90 <= averageGrade <= 92:
                 print("Very Good! Your grade point is 1.75")
-                print("Your final grade is " + str(averageGrade))
             elif 87 <= averageGrade <= 89:
                 print("Good! Your grade point is 2.00")
-                print("Your final grade is " + str(averageGrade))
             elif 84 <= averageGrade <= 86:
                 print("Satisfactory! Your grade point is 2.25")
-                print("Your final grade is " + str(averageGrade))
             elif 81 <= averageGrade <= 83:
                 print("Fairly Satisfactory! Your grade point is 2.50")
-                print("Your final grade is " + str(averageGrade))
             elif 78 <= averageGrade <= 80:
                 print("Fair! Your grade point is 2.75")
-                print("Your final grade is " + str(averageGrade))
             elif 75 <= averageGrade <= 77:
                 print("Passed! Your grade point is 3.00")
-                print("Your final grade is " + str(averageGrade))
             else:
                 print("Failed! Your grade point is 5.00")
-                print("Your final grade is " + str(averageGrade))
